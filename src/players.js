e5c29e93a3328e591d2bfedc99d5ea747fa330f3
Rayaninha <salesrayane11@gmail.com>
2024-06-20 11:45:03 -0300

    feat: player selection

---
+++ src/players.js
@@ -0,0 +1,41 @@
+export const mario = {
+  name: "Mario",
+  speed: 4,
+  handling: 3,
+  power: 3,
+}
+
+export const peach = {
+  name: "Peach",
+  speed: 3,
+  handling: 4,
+  power: 2,
+}
+
+export const yoshi = {
+  name: "Yoshi",
+  speed: 3,
+  handling: 4,
+  power: 2,
+}
+
+export const bowser = {
+  name: "Bowser",
+  speed: 3,
+  handling: 4,
+  power: 2,
+}
+
+export const luigi = {
+  name: "Luigi",
+  speed: 3,
+  handling: 4,
+  power: 2,
+}
+
+export const donkeyKong = {
+  name: "Donkey Kong",
+  speed: 3,
+  handling: 4,
+  power: 2,
+}
